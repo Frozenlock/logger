a779017f34adf65568e42fe8b7a76bcd9d960f58
Frozenlock <frozenlock@gmail.com>
2013-03-12 12:41:33 -0400

    temporary fix for clojure regexp equality bug

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.frozenlock/logger "2.0.26"
+(defproject org.clojars.frozenlock/logger "2.0.27"
   :description "Automatic logger for a BACnet network"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
